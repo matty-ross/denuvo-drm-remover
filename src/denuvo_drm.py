b911d7d3927a4d191cbd13524224370dedff3655
Matty <54020396+matty-ross@users.noreply.github.com>
2026-01-30 21:03:06 +0100

    Mapping denuvo to original imports.

--- src/denuvo_drm.py
+++ src/denuvo_drm.py
@@ -5,7 +5,7 @@ import pefile
 
 class DenuvoDRM:
 
-    def __init__(self, config_path: str, pe_path: str, ):
+    def __init__(self, config_path: str, pe_path: str):
         self._config = self._load_config(config_path)
         self._pe = pefile.PE(pe_path, fast_load=True)
 
@@ -14,6 +14,11 @@ class DenuvoDRM:
         self._pe.OPTIONAL_HEADER.CheckSum = self._pe.generate_checksum()
         self._pe.write(new_pe_path)
 
+
+    def fix_imports(self) -> None:
+        imports = self._map_denuvo_to_original_imports()
+        # TODO
+    
 
     def fix_pe_header(self) -> None:
         optional_header: dict[str, str] = self._config['optional_header']
@@ -46,3 +51,29 @@ class DenuvoDRM:
     def _get_pe_data_directory(self, name: str) -> pefile.Structure:
         index = pefile.DIRECTORY_ENTRY[name]
         return self._pe.OPTIONAL_HEADER.DATA_DIRECTORY[index]
+    
+
+    def _map_denuvo_to_original_imports(self) -> dict[int, int]:
+        denuvo_imports = {}
+        denuvo_import_directory = self._pe.parse_import_directory(rva=0x7E892F8, size=0x244) # TODO
+        for denuvo_import_descriptor in denuvo_import_directory:
+            for denuvo_import in denuvo_import_descriptor.imports:
+                dll = denuvo_import_descriptor.dll.lower()
+                function = denuvo_import.ordinal if denuvo_import.import_by_ordinal else denuvo_import.name.lower()
+                denuvo_imports[denuvo_import.address] = (dll, function)
+
+        original_imports = {}
+        original_import_directory = self._pe.parse_import_directory(rva=0xB22AF0, size=0x2D0) # TODO
+        for original_import_descriptor in original_import_directory:
+            for original_import in original_import_descriptor.imports:
+                dll = original_import_descriptor.dll.lower()
+                function = original_import.ordinal if original_import.import_by_ordinal else original_import.name.lower()
+                original_imports[(dll, function)] = original_import.address
+
+        imports = {}
+        for denuvo_address, (dll, function) in denuvo_imports.items():
+            original_address = original_imports.get((dll, function))
+            if original_address is not None:
+                imports[denuvo_address] = original_address
+
+        return imports
--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ def main() -> None:
     new_pe_path = filedialog.asksaveasfilename(title="New PE file", defaultextension='exe')
     
     denuvo_drm = DenuvoDRM(config_path, pe_path)
+    denuvo_drm.fix_imports()
     denuvo_drm.fix_pe_header()
     denuvo_drm.rename_sections()
     denuvo_drm.save_pe(new_pe_path)
